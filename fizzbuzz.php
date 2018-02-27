f9c9c2156fb0c2128dc83c9860cb75413421e1f1
Caleb Rogers <caleb.rogers@gmail.com>
2018-02-26 18:12:24 -0800

    My fizzbuzz implementation using PHP

---
+++ fizzbuzz.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * FizzBuzz implementation in PHP
+ * Remember, you need to PHP and a web server running to execute this
+ */
+for ($count = 1; $count <=100; $count++){
+    if($count % 3 == 0){
+        print("Fizz");
+    }elseif($count % 5 == 0){
+        print("Buzz");
+    } else {
+        print($count);
+    }
+    print("<br />");
+}
+?>
