0fe586b9d3fbd859d7cc4e0a869256849dcbc31e
Caleb Rogers <caleb.rogers@gmail.com>
2018-02-26 18:13:30 -0800

    Fixed some spacing I didn't like

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -4,9 +4,9 @@
  * Remember, you need to PHP and a web server running to execute this
  */
 for ($count = 1; $count <=100; $count++){
-    if($count % 3 == 0){
+    if($count % 3 == 0) {
         print("Fizz");
-    }elseif($count % 5 == 0){
+    } elseif($count % 5 == 0) {
         print("Buzz");
     } else {
         print($count);
